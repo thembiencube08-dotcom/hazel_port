f3b8dcc7738c67bbb16b4d5c8dae037e15063f28
Thembelihle Ncube <thembiencube08@gmail.com>
2026-08-04 15:23:04 +0200

    updated the import state on projects section

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import {  useRef } from 'react';
 import styled from 'styled-components';
 import { motion, useInView } from 'framer-motion';
 import { GitBranch, ExternalLink } from 'lucide-react';
